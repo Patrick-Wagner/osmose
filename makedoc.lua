16aee7a68ccf6bd07da7e5f3953cc19ae5a9ef4f
Renaud Kern <renaud.kern@gmail.com>
2014-03-13 11:59:14 +0100

    add docs

---
+++ makedoc.lua
@@ -0,0 +1,16 @@
+local lut = require 'lut'
+
+lut.Doc.make {
+  sources = {
+    'lib/osmose.lua',
+    'lib/osmose/Model.lua',
+    'lib/osmose/Eiampl.lua',
+    'lib/osmose/Glpk.lua', 
+    'lib/osmose/Graph.lua',
+    'lib/osmose/Project.lua',
+  },
+  target = 'docs',
+  format = 'html',
+  header = [[  <h1>Osmose documentation</h1> ]]
+}
+
