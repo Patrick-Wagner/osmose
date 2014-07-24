73ae6befcabc6d67b81c32c42e531c94e54f1154
fazlollahi <samira.fazlollahi@epfl.ch>
2014-07-24 12:38:26 +0200

    I add a new function, "AddMerUnits", for MER definition and I removed lib.addMerunits from Glpk.lua

---
+++ lib/osmose/AddMerUnits.lua
@@ -0,0 +1,106 @@
+-- This function add 2 Units to solve the MER Objective : Default HeatCascade Unit Hot (DHCU_h) and
+-- Default HeatCascade Unit Cold (DHCU_c). Each of them has 1 stream, hot and cold.
+
+-- @author: Samira Fazlollahi (samira.fazlollahi@a3.epfl.ch)
+
+-- @copyright IPESE
+
+-- @param: Project.units, Project.project_name
+
+-- @release 0.1
+
+-- @return: the MER units
+
+-- @status _proposed
+
+
+
+local lub 	= require 'lub'
+local lib 	= lub.class 'osmose.AddMerUnits'
+
+
+function lib.new(units, project_name)
+		table.insert(units,{ name="DHCU_h", force_use=0, 
+		Fmin=0, 
+		Fmax=100000, 
+		--Cost1=1000,
+		--Cost2=1000,
+    massStreams={},
+    resourceStreams = {},
+		layers={DefaultImpact={name = "DefaultImpact", type = "Costing"},
+      DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
+      DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
+      DefaultMechPower={name = "DefaultMechPower", type = "Costing"}},
+
+		streams={{name="DHCS_h", unitName="DHCU_h",
+					Tin				=function() return 99999 end, 
+					Tout			=function() return 99998 end, 
+					Tin_corr	=function() return 99999 end, 
+					Tout_corr	=function() return 99998 end, 
+					Hin 			=function() return 1000 end, 
+					Hout 			=function() return 0 end, 
+					isHot			=function() return true end,
+					load 			={},
+					draw			=false }},
+     costStreams={
+       {layerName = "DefaultOpCost", name = "DHCS_h_Cost", unitName="DHCU_h",
+         coefficient1 = function() return 100 end, 
+         coefficient2 = function() return 100 end},
+       {layerName = "DefaultInvCost", name = "DHCS_h_Cinv", unitName="DHCU_h",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end},
+       {layerName = "DefaultMechPower", name = "DHCS_h_Power", unitName="DHCU_h",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end},
+       {layerName = "DefaultImpact", name = "DHCS_h_Impact", unitName="DHCU_h",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end}}
+
+		})
+
+
+	table.insert(units,{name="DHCU_c", force_use=0, 
+		Fmin=0, 
+		Fmax=100000, 
+		--Cost1 = 1000,
+		--Cost2 = 1000,
+    massStreams ={},
+    resourceStreams = {},
+		layers={DefaultImpact={name = "DefaultImpact", type = "Costing"},
+      DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
+      DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
+      DefaultMechPower={name = "DefaultMechPower", type = "Costing"}},
+
+		streams={{name="DHCS_c", unitName="DHCU_c",  
+					Tin 			=function() return 100 end, 
+					Tout 			=function() return 105 end, 
+					Tin_corr 	=function() return 100 end, 
+					Tout_corr =function() return 105 end, 
+					Hin 			=function() return 0 end, 
+					Hout 			=function() return 1000 end, 
+					isHot			=function() return false end,
+					load 			={},
+					draw			=false }},
+    
+    costStreams={
+       {layerName = "DefaultOpCost", name = "DHCS_c_Cost", unitName="DHCU_c",
+         coefficient1 = function() return 100 end, 
+         coefficient2 = function() return 100 end},
+       {layerName = "DefaultInvCost", name = "DHCS_c_Cinv", unitName="DHCU_c",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end},
+       {layerName = "DefaultMechPower", name = "DHCS_c_Power", unitName="DHCU_c",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end},
+       {layerName = "DefaultImpact", name = "DHCS_c_Impact", unitName="DHCU_c",
+         coefficient1 = function() return 0 end, 
+         coefficient2 = function() return 0 end}}
+      
+     
+		})
+
+	return units
+end
+
+
+return lib
--- lib/osmose/Glpk.lua
+++ lib/osmose/Glpk.lua
@@ -139,7 +139,10 @@ function lib.generateDataWithTimes(project, periode)
 		print('-------------------------------------------------------------------------------------------')
     print('MER Objective')
 		print('-------------------------------------------------------------------------------------------')
-		project.units[periode] = lib.addMerUnits(project.units[periode], project.name)
+    
+    local add_MER_units = require('osmose.AddMerUnits')
+		project.units[periode] = add_MER_units(project.units[periode], project.name)
+    
 	end
 
 	local times = {}
@@ -375,92 +378,6 @@ function lib.generateDataWithTimes(project, periode)
 	})
 
 end
-
-
-
--- This function add 2 Units to solve the MER Objective : Default HeatCascade Unit Hot (DHCU_h) and
--- Default HeatCascade Unit Cold (DHCU_c). Each of them has 1 stream, hot and cold.
-function lib.addMerUnits(units, project_name)
-	table.insert(units,{ name="DHCU_h", force_use=0, 
-		Fmin=0, 
-		Fmax=100000, 
-		--Cost1=1000,
-		--Cost2=1000,
-    massStreams={},
-		layers={DefaultImpact={name = "DefaultImpact", type = "Costing"},
-      DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
-      DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
-      DefaultMechPower={name = "DefaultMechPower", type = "Costing"}},
-
-		streams={{name="DHCS_h", unitName="DHCU_h",
-					Tin				=function() return 99999 end, 
-					Tout			=function() return 99998 end, 
-					Tin_corr	=function() return 99999 end, 
-					Tout_corr	=function() return 99998 end, 
-					Hin 			=function() return 1000 end, 
-					Hout 			=function() return 0 end, 
-					isHot			=function() return true end,
-					load 			={},
-					draw			=false }},
-     costStreams={
-       {layerName = "DefaultOpCost", name = "DHCS_h_Cost", unitName="DHCU_h",
-         coefficient1 = function() return 100 end, 
-         coefficient2 = function() return 100 end},
-       {layerName = "DefaultInvCost", name = "DHCS_h_Cinv", unitName="DHCU_h",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end},
-       {layerName = "DefaultMechPower", name = "DHCS_h_Power", unitName="DHCU_h",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end},
-       {layerName = "DefaultImpact", name = "DHCS_h_Impact", unitName="DHCU_h",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end}}
-
-		})
-
-
-	table.insert(units,{name="DHCU_c", force_use=0, 
-		Fmin=0, 
-		Fmax=100000, 
-		--Cost1 = 1000,
-		--Cost2 = 1000,
-    massStreams ={},
-		layers={DefaultImpact={name = "DefaultImpact", type = "Costing"},
-      DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
-      DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
-      DefaultMechPower={name = "DefaultMechPower", type = "Costing"}},
-
-		streams={{name="DHCS_c", unitName="DHCU_c",  
-					Tin 			=function() return 100 end, 
-					Tout 			=function() return 105 end, 
-					Tin_corr 	=function() return 100 end, 
-					Tout_corr =function() return 105 end, 
-					Hin 			=function() return 0 end, 
-					Hout 			=function() return 1000 end, 
-					isHot			=function() return false end,
-					load 			={},
-					draw			=false }},
-    
-    costStreams={
-       {layerName = "DefaultOpCost", name = "DHCS_c_Cost", unitName="DHCU_c",
-         coefficient1 = function() return 100 end, 
-         coefficient2 = function() return 100 end},
-       {layerName = "DefaultInvCost", name = "DHCS_c_Cinv", unitName="DHCU_c",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end},
-       {layerName = "DefaultMechPower", name = "DHCS_c_Power", unitName="DHCU_c",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end},
-       {layerName = "DefaultImpact", name = "DHCS_c_Impact", unitName="DHCU_c",
-         coefficient1 = function() return 0 end, 
-         coefficient2 = function() return 0 end}}
-      
-     
-		})
-
-	return units
-end
-
 
 -- This function return all the mininum streams of the *units* and sort them ascendingly.
 function lib.streamsTinWithTimes(units, periode, times)
