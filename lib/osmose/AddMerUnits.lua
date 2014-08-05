50e260c79c2c5b63aa24a240b61480beaf48710f
fazlollahi <samira.fazlollahi@epfl.ch>
2014-08-05 17:19:02 +0200

    Update MER objective function

--- lib/osmose/AddMerUnits.lua
+++ lib/osmose/AddMerUnits.lua
@@ -21,23 +21,62 @@ local lib 	= lub.class 'osmose.AddMerUnits'
 
 function lib.new(project,units)
  
- -- recover heat cascade layers
- local HClayersMER = {}
- local layersAll ={}
+ local layersAll = {}
+  -- Recover all layers
+  local heatCascadeLayer = {}
+	local massBalanceLayer = {}
+  local resourceBalanceLayer = {}
+  local costingLayer={}
+
+  
+
 	for m, model in ipairs(project.models) do
 		for layerName, layer in pairs(model.layers) do
-      if layer ~= nil and layer.type == 'HeatCascade' then
-          table.insert(HClayersMER, layerName) 
-          table.insert(layersAll, layer)
+      ---add costing Layers (samira.fazlollahi@a3.epfl.ch) 
+      local fullName
+      if layer ~= nil and layer.type == 'Costing' then
+			  fullName = layerName
+	  elseif layer ~= nil and layer.type == 'HeatCascade' then
+			  fullName = layerName
+      else
+        fullName =  'layers_'..layerName
+      end
+			local layerFound = 0
+			for i,l in ipairs(layersAll) do
+				if l==fullName then
+					layerFound = 1
+				end
+			end
+			if layerFound == 0 then
+				table.insert(layersAll, fullName)
+
+				if layer.type == 'MassBalance' then
+					table.insert(massBalanceLayer, fullName)
+          ---add ResourceBalance Layers (samira.fazlollahi@a3.epfl.ch) 
+        elseif layer.type == 'ResourceBalance' then
+					table.insert(resourceBalanceLayer, fullName)
+          ---add costing Layers (samira.fazlollahi@a3.epfl.ch) 
+        elseif layer.type == 'Costing' then
+          table.insert(costingLayer, fullName)
+          ---add HeatCascade Layers (samira.fazlollahi@a3.epfl.ch) 
+         elseif layer.type == 'HeatCascade' then
+          table.insert(heatCascadeLayer, fullName) 
+           
+				else
+					print(string.format("Layer of type '%s' is not recognized.", layer.type))
+					print("Valid layer types are : Costing, HeatCascade, MassBalance, ResourceBalance")
+					os.exit()
+				end
 			end
 		end
 	end
+	
 
-  
-  for eachlayerName, layer in pairs(layersAll) do 
+  -- add hot and cold utility
+  for eachlayerName, layer in pairs(heatCascadeLayer) do 
     
     -- add hot utility   
-		table.insert(units,{name=layer.name.."_DHCU_h", force_use=0, 
+		table.insert(units,{name=layer.."_DHCU_h", force_use=0, 
     fFmin = function() return 0 end,
     fFmax = function() return 100000 end,
 		--Cost1=1000,
@@ -49,30 +88,30 @@ function lib.new(project,units)
       DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
       DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
       DefaultMechPower={name = "DefaultMechPower", type = "Costing"},
-      [layer.name] = layer },      
+      [layer] = {name = layer, type = "HeatCascade"}},      
     
-		streams={{name=layer.name.."_DHCS_h", unitName=layer.name.."_DHCU_h",
+		streams={{name=layer.."_DHCS_h", unitName=layer.."_DHCU_h",
 					Tin				=function() return 99999 end, 
 					Tout			=function() return 99998 end, 
 					Tin_corr	=function() return 99999 end, 
 					Tout_corr	=function() return 99998 end, 
 					Hin 			=function() return 1000 end, 
 					Hout 			=function() return 0 end, 
-          layerName = layer.name,
+          layerName = layer,
 					isHot			=function() return true end,
 					load 			={},
 					draw			=false }},
      costStreams={
-       {layerName = "DefaultOpCost", name = layer.name.."_DHCS_h_Cost", unitName=layer.name.."_DHCU_h",
+       {layerName = "DefaultOpCost", name = layer.."_DHCS_h_Cost", unitName=layer.."_DHCU_h",
          coefficient1 = function() return 100 end, 
          coefficient2 = function() return 100 end},
-       {layerName = "DefaultInvCost", name = layer.name.."_DHCS_h_Cinv", unitName=layer.name.."_DHCU_h",
+       {layerName = "DefaultInvCost", name = layer.."_DHCS_h_Cinv", unitName=layer.."_DHCU_h",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end},
-       {layerName = "DefaultMechPower", name = layer.name.."_DHCS_h_Power", unitName=layer.name.."_DHCU_h",
+       {layerName = "DefaultMechPower", name = layer.."_DHCS_h_Power", unitName=layer.."_DHCU_h",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end},
-       {layerName = "DefaultImpact", name = layer.name.."_DHCS_h_Impact", unitName=layer.name.."_DHCU_h",
+       {layerName = "DefaultImpact", name = layer.."_DHCS_h_Impact", unitName=layer.."_DHCU_h",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end}}
 
@@ -80,7 +119,7 @@ function lib.new(project,units)
 
 
   -- add cold utility
-	table.insert(units,{name=layer.name.."_DHCU_c", force_use=0, 
+	table.insert(units,{name=layer.."_DHCU_c", force_use=0, 
     fFmin = function() return 0 end,
     fFmax = function() return 100000 end,
 		--Cost1 = 1000,
@@ -92,31 +131,31 @@ function lib.new(project,units)
       DefaultOpCost={name = "DefaultOpCost", type = "Costing"},
       DefaultInvCost={name = "DefaultInvCost", type = "Costing"},
       DefaultMechPower={name = "DefaultMechPower", type = "Costing"},
-      [layer.name] = layer},
+      [layer] = {name = layer, type = "HeatCascade"}},
       
-		streams={{name=layer.name.."_DHCS_c", unitName=layer.name.."_DHCU_c",  
+		streams={{name=layer.."_DHCS_c", unitName=layer.."_DHCU_c",  
 					Tin 			=function() return 100 end, 
 					Tout 			=function() return 105 end, 
 					Tin_corr 	=function() return 100 end, 
 					Tout_corr =function() return 105 end, 
 					Hin 			=function() return 0 end, 
 					Hout 			=function() return 1000 end, 
-          layerName = layer.name,
+          layerName = layer,
 					isHot			=function() return false end,
 					load 			={},
 					draw			=false }},
     
     costStreams={
-       {layerName = "DefaultOpCost", name = layer.name.."_DHCS_c_Cost", unitName=layer.name.."_DHCU_c",
+       {layerName = "DefaultOpCost", name = layer.."_DHCS_c_Cost", unitName=layer.."_DHCU_c",
          coefficient1 = function() return 100 end, 
          coefficient2 = function() return 100 end},
-       {layerName = "DefaultInvCost", name = layer.name.."_DHCS_c_Cinv", unitName=layer.name.."_DHCU_c",
+       {layerName = "DefaultInvCost", name = layer.."_DHCS_c_Cinv", unitName=layer.."_DHCU_c",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end},
-       {layerName = "DefaultMechPower", name = layer.name.."_DHCS_c_Power", unitName=layer.name.."_DHCU_c",
+       {layerName = "DefaultMechPower", name = layer.."_DHCS_c_Power", unitName=layer.."_DHCU_c",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end},
-       {layerName = "DefaultImpact", name = layer.name.."_DHCS_c_Impact", unitName=layer.name.."_DHCU_c",
+       {layerName = "DefaultImpact", name = layer.."_DHCS_c_Impact", unitName=layer.."_DHCU_c",
          coefficient1 = function() return 0 end, 
          coefficient2 = function() return 0 end}}
 
