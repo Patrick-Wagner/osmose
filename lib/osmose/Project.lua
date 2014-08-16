0ebffe5562f25ac7b35bb591107ea6f1a5e3618a
Renaud Kern <renaud.kern@gmail.com>
2014-08-16 13:36:38 +0200

    prepare Project for for optimization

--- lib/osmose/Project.lua
+++ lib/osmose/Project.lua
@@ -10,6 +10,11 @@
 local lub   = require 'lub'
 local lib   = lub.class 'osmose.Project'
 local lfs = require "lfs"
+local helper = require 'osmose.helpers.projectHelper'
+local Eiampl = require 'osmose.Eiampl'
+local Glpk   = require 'osmose.Glpk'
+local Graph  = require 'osmose.Graph'
+local socket = require("socket")
 
 
 -- Function for metatable.
@@ -109,8 +114,6 @@ function lib:load(...)
       model = modelPath (name)
     end
 
-
-    local helper = require 'osmose.helpers.projectHelper'
     helper.loadUndeclaredTags(model)
 
     -- Get the path of the file that has launched the script.
@@ -149,6 +152,81 @@ function lib:periode(periode)
   return self.periodes[periode]
 end
 
+function lib:getUnit(name, periode)
+  local periode = periode or 1
+  for i,u in ipairs(self.units[periode] or {}) do    
+    if u.name == name or u.shortName == name then
+      return u
+    end
+  end
+end
+
+function lib:getStream(name, periode)
+  local periode = periode or 1
+  for i,u in ipairs(self.units[periode] or {}) do
+    for j,s in  ipairs(u.streams or {}) do
+      if s.name == name or s.shortName == name then
+        return s
+      end
+    end
+  end
+end
+
+function lib:getTag(name, periode, time)
+  local periode = periode or 1
+  local time = time or 1
+
+  for i,m in ipairs(self.models or {}) do
+    if m.present(name) then
+      m.periode = periode
+      m.time = time
+      return m[name]
+    end
+  end
+end
+
+function lib:setTag(name, value, periode, time)
+  local periode = periode or 1
+  local time = time or 1
+
+  for i,m in ipairs(self.models or {}) do
+    if m.present(name) then
+      m.periode = periode
+      m.time = time
+      m[name] = value
+      return m[name]
+    end
+  end
+end
+
+function lib:call(str)
+  if str==nil or str=='' then return nil end
+  local str = lub.strip(str)
+  local args = lub.split(str,',')
+  
+  local fct = lub.strip(args[1])
+  if fct == 'getTag' then
+    if table.getn(args) <=1 then return nil end
+    local name = args[2]
+    local periode = tonumber(args[3] or '1')
+    local time = tonumber(args[4] or '1')
+    --return self[fct](self, name, periode, time)
+    return lib.getTag(self,name, periode, time)
+  elseif fct == 'setTag' then
+    local name = args[2]
+    local value = tonumber(args[3]) or args[3]
+    local periode = tonumber(args[4] or '1')
+    local time = tonumber(args[5] or '1')
+    return lib.setTag(self,name, value, periode, time)
+  elseif fct == 'solve' then
+    Glpk(self)
+    return true
+    --Graph(self, {format='svg'})
+  else
+    return nil
+  end
+end
+
 --[[
   Solve the project with Glpk by default and do graphs.
 
@@ -168,16 +246,16 @@ end
 --]]
 function lib:solve(args)
   local args = args or {}
-  local Eiampl = require 'osmose.Eiampl'
-  local Glpk   = require 'osmose.Glpk'
-  local Graph  = require 'osmose.Graph'
 
+  -- prepare project for Eiampl
   local self = Eiampl(self)
 
+  -- solve project
   if args.solver=='GLPK' or args.solver==nil then
     Glpk(self)
   end
 
+  -- write graph if necessary
   if args.graph~=false and self.solved==true then
     local format = nil
     if args.graph then
@@ -188,5 +266,91 @@ function lib:solve(args)
 
   return self
 end
+
+function lib:optimize(args)
+  
+  local sourceDir = self.sourceDir
+  local software = args['software']
+  local cmd = ''
+  local tmpDire = ''
+  if software == nil then
+    print('Yous must specify a software name for opimization.')
+    return nil
+  elseif software == 'dakota' then
+    local dakota_helper = require 'osmose.helpers.dakotaHelper'
+
+    -- create the directory that will run dakota
+    tmpDir = ('./results/'..self.name..'/run_'..self.run..'/dakota')
+    lub.makePath(tmpDir)
+
+    -- prepare objective file for Dakota
+    cmd = dakota_helper.prepareFiles(tmpDir,sourceDir, args)
+    cmd = cmd .." > "..tmpDir.."/output.txt"
+    cmd = cmd .." && lua "..tmpDir.."/stop.lua"
+  end
+
+  -- prepare project with Eiampl
+  local project = Eiampl(self)
+
+  print(lub.plat())
+  if lub.plat() == 'macosx' or lub.plat() == 'linux' then
+    os.execute "lsof -t -i  tcp:3333 | xargs kill"
+  end
+  local server = assert(socket.bind("*", 3333))
+
+
+  local dakota_output = assert(io.popen(cmd,"w"))
+  
+
+  function listen(server, tmpDir, project)
+    local client = server:accept()
+    client:settimeout(10)
+    local line, err, partial = client:receive("*l")
+    --print('listen', line, err, partial)
+    if line=="stop" or partial=="stop" then
+      print("Optimization finished")
+      print("Result direcory is", tmpDir)
+      client:close()
+      server:close()
+      return "stop"
+    end
+    local rslt = lib.call(project,line)
+    if type(rslt) == 'function' then
+      rslt = rslt()
+    end
+    --print('result', rslt)
+    if rslt then 
+      client:send(tostring(rslt).."\n")
+      client:close()
+    else
+      client:close()
+    end
+  end
+
+  while true do
+    if listen(server,tmpDir, project) == "stop" then break end
+  end
+
+end
+
+
+function lib:postCompute(name)
+  if self.sourceDir == nil then
+    local sourcePath = debug.getinfo(2).source:sub(2)
+    self.sourceDir = sourcePath:match("(.*[/\\])")
+  end
+
+  local full_path = (self.sourceDir or "")..name..".lua"
+  if  io.open(full_path) == nil then  
+    return nil
+  end
+
+  local fct = loadfile(full_path)()
+
+  return _G[name](self)
+end
+
+
+
 
 return lib
--- test/osmose/Project_test.lua
+++ test/osmose/Project_test.lua
@@ -5,6 +5,13 @@ local lut    = require 'lut'
 
 local should = lut.Test 'osmose.Project'
 
+function initProject()
+	local p1 = osmose.Project('LuaJam', 'MER')
+	p1:load({cip = "ET.Cip"})
+	local Eiampl = require 'osmose.Eiampl'
+	return Eiampl(p1)
+end
+
 function should.createNewObject()
 	local p1 = osmose.Project('LuaJam', 'MER')
 	assertEqual('LuaJam', p1.name)
@@ -40,5 +47,97 @@ function should.solve()
 	assertEqual(p1, solution)
 end
 
+function should.getStream()
+	local p1=initProject()
+
+	local s = p1:getStream('dot_cold')
+	assertEqual('dot_cold', s.shortName)
+
+	s=p1:getStream('LuaJam_cip_CipUnit_dot_cold')
+	assertEqual('LuaJam_cip_CipUnit_dot_cold', s.name)
+
+	assertNil(p1:getStream('dot_cold',2))
+end
+
+function should.getUnit()
+	local p1=initProject()
+
+	local u = p1:getUnit("CipUnit")
+	assertEqual('CipUnit', u.shortName)
+
+	u = p1:getUnit("LuaJam_cip_CipUnit")
+	assertEqual('LuaJam_cip_CipUnit', u.name)
+end
+
+function should.getTag()
+	local p1=initProject()
+
+	local tag = p1:getTag('tank_temp')
+	assertEqual(85, tag)
+
+	tag = p1:getTag('raw_water_flow')
+	assertEqual(5, tag())
+
+	assertEqual( 40, p1:getTag('cleaning_agent_load')(),1)
+
+	assertNil(p1:getTag('nothing'))
+
+	assertEqual(85, p1.getTag(p1, 'tank_temp'))
+end
+
+
+
+
+function should.setTag()
+	local p1=initProject()
+
+	local tag = p1:setTag('tank_temp', 75)
+	assertEqual(75, tag)
+
+	tag = p1:setTag('foo', 1)
+	assertNil(tag)
+end
+
+function should.call()
+	local p1=initProject()
+
+	local val = p1:call("getTag,tank_temp")
+	assertEqual(85, val)
+
+	val = p1:call("getTag,raw_water_flow")
+	assertEqual(5, val())
+
+	val = p1:call("setTag,tank_temp, 70")
+	assertEqual(70, val)
+end
+
+function should.callWithNilValue()
+	local p1=initProject()
+
+	local val = p1:call("")
+	assertNil(val)
+
+	val=p1:call()
+	assertNil(val)
+
+	val=p1:call("getTag")
+	assertNil(val)
+
+
+	val=p1:call("getTag,foo")
+	assertNil(val)
+end
+
+function should.optimize()
+	local p1 = initProject()
+
+	assertNil(p1:optimize({}))
+end
+
+function should.postCompute()
+	local p1 = initProject()
+
+	assertNil(p1:postCompute(""))
+end
 
 should:test()
